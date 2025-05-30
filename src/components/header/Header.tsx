a0e1a4adde02ef4ae92d5fcd2288208ca8d4778e
robshow <robsonaraujocarmo@hotmail.com>
2025-05-30 10:04:13 -0400

    feat: add data hub menu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,6 +7,10 @@ import { Button } from "@mui/material";
 import { Typography } from "@mui/material";
 import Ray from "@/app/icons/Ray";
 import Link from "next/link";
+import DynamicFormOutlinedIcon from "@mui/icons-material/DynamicFormOutlined";
+import HideOnMobile from "../hideOnMobile/HideOnMobile";
+import Space from "../space/Space";
+import HideOnDesktop from "../hideOnDesktop/HideOnDesktop";
 
 export default function Header() {
   return (
@@ -14,15 +18,52 @@ export default function Header() {
       className={`flex items-center px-[160px] max-md:px-4 justify-between  border-b border-lightGrey  h-[72px] "
       }`}
     >
-      <Link href="/">
-        <Image
-          src={logo}
-          alt="Data PANP logotipo"
-          width={140}
-          className="cursor-pointer"
-          priority
-        />
-      </Link>
+      <div className="flex items-center justify-center">
+        <Link href="/">
+          <Image
+            src={logo}
+            alt="Data PANP logotipo"
+            width={140}
+            className="cursor-pointer"
+          />
+        </Link>
+        <Space right={10} />
+        <Link href="https://data-panp-tests.vercel.app/">
+          <HideOnMobile>
+            <Typography
+              mt={1}
+              sx={{ cursor: "pointer" }}
+              variant="body1"
+              fontWeight="700"
+              textTransform="capitalize"
+              fontSize="12px"
+              className="flex items-center "
+            >
+              <DynamicFormOutlinedIcon
+                style={{ fontSize: "20px" }}
+                className="mr-1"
+              />
+              Data Hub
+            </Typography>
+          </HideOnMobile>
+          <HideOnDesktop>
+            <Typography
+              mt={1}
+              sx={{ cursor: "pointer" }}
+              variant="body1"
+              fontWeight="700"
+              textTransform="capitalize"
+              fontSize="14px"
+              className="flex items-center flex-col justify-center"
+            >
+              <DynamicFormOutlinedIcon style={{ fontSize: "24px" }} />
+              {"  "}
+              Hub
+            </Typography>
+          </HideOnDesktop>
+        </Link>
+        <Space right={4} />
+      </div>
 
       <Button
         variant="contained"
